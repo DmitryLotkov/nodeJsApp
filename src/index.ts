96f3637b786b55e2e2a90e85a010a3fab56cee95
DmitryLotkov <dmitry.lotkov9@gmail.com>
2023-01-05 20:45:13 +0300

    fixed message4

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import express, {Request, Response} from 'express'
 const app = express()
 const port = process.env.PORT || 5000
-
-app.get('/', (req: Request, res:Response) => {
-    let helloMessage = 'Hello World;)'
-    res.send(helloMessage)
+const products = [{title:"tomato"}, {title:"orange"}]
+const addresses = [{value: "Nazalejnasti 12"}, {value: "Selickaga 11"}]
+app.get('/products', (req: Request, res:Response) => {
+    res.send(products)
+})
+app.get('/addresses', (req: Request, res:Response) => {
+    res.send(addresses)
 })
 
 app.listen(port, () => {
